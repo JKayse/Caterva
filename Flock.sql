15bb044625431ac1696f66cc06efaca4c1d2ab6a
alexcheng996 <videogame5678@gmail.com>
2014-03-17 00:26:04 -0500

    Creted a sql file that should create the database with all the tables and necessary attributes.  its empty and needs to be filled with stuff.

---
+++ Flock.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.6deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 17, 2014 at 05:22 AM
+-- Server version: 5.5.35-0ubuntu0.13.10.2
+-- PHP Version: 5.5.3-1ubuntu2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `Flock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `EventRequest`
+--
+CREATE DATABASE Flock;
+USE Flock;
+
+CREATE TABLE IF NOT EXISTS `EventRequest` (
+  `EventRequestId` int(11) NOT NULL AUTO_INCREMENT,
+  `EventId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`EventRequestId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Events`
+--
+
+CREATE TABLE IF NOT EXISTS `Events` (
+  `EventId` int(11) NOT NULL AUTO_INCREMENT,
+  `EventName` varchar(50) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `StartTime` datetime NOT NULL,
+  `EndTime` datetime NOT NULL,
+  `EventDescription` varchar(500) NOT NULL,
+  PRIMARY KEY (`EventId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `FriendRequest`
+--
+
+CREATE TABLE IF NOT EXISTS `FriendRequest` (
+  `FriendRequestId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) NOT NULL,
+  `FriendId` int(11) NOT NULL,
+  PRIMARY KEY (`FriendRequestId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `FriendsList`
+--
+
+CREATE TABLE IF NOT EXISTS `FriendsList` (
+  `FriendId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `UserFriendId` int(11) NOT NULL,
+  PRIMARY KEY (`FriendId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `GroupList`
+--
+
+CREATE TABLE IF NOT EXISTS `GroupList` (
+  `GroupListId` int(11) NOT NULL AUTO_INCREMENT,
+  `GroupId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`GroupListId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Groups`
+--
+
+CREATE TABLE IF NOT EXISTS `Groups` (
+  `GroupId` int(11) NOT NULL AUTO_INCREMENT,
+  `GroupName` varchar(50) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`GroupId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `GuestList`
+--
+
+CREATE TABLE IF NOT EXISTS `GuestList` (
+  `GuestListId` int(11) NOT NULL AUTO_INCREMENT,
+  `EventId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`GuestListId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(100) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `PasswordSalt` varchar(100) NOT NULL,
+  `Description` varchar(500) DEFAULT NULL,
+  `PictureName` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `UserName` (`UserName`,`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
