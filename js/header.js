8ae994368e2cc8e87d0dfcad5b99be5c09e1bffa
JKayse <jkayse@smu.edu>
2014-03-21 15:35:10 -0500

    Added the sign in/sign up/sign out functions. Need the database functions to test.

---
+++ js/header.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function() {
+
+    $(document).on('submit', "#signInArea", signIn);
+    $(document).on('click', "header #signedIn a", signout);
+    $(document).on('click', "header img", goToCorrectPage);
+    $(document).on('click', "header h1", goToCorrectPage);
+
+    /*$.ajax({url:"api/LoginStatus", success: function(json){
+        if(json !== 'null'){
+            json = JSON.parse(json);
+            $("#signedIn h2").html("Welcome!");
+            $("#signInUsername").val("");
+            $("#signInPass").val("");
+            $("#signIn").css("display", "none");
+            $("#signedIn").css("display", "block");                
+        }
+        else{
+            return;
+        }
+    }});   
+*/
+});
+
+
+function goToCorrectPage(){
+    /*$.ajax({url:"api/LoginStatus", success: function(json){
+        if(json !== 'null'){
+             window.location = "main.html";
+        }
+        else{
+            window.location = "index.html";
+        }
+    }});  
+*/
+}
+
+
+function signout(){
+    /*$.ajax({
+            type: "POST",
+            url: "api/Logout",
+            success: function(){
+                $("#signIn").css("display", "block");
+                $("#signedIn").css("display", "none");
+    }});
+*/
+
+}
+
+function signIn(){
+    event.preventDefault();
+    /*$.ajax({
+            type: "POST",
+            url: "api/Login",
+            data: {
+                username: $("#signInUsername").val(),
+                password: $("#signInPass").val()
+            },
+            success: function(json){
+                if(json === 'null'){
+                    alert("The information entered was not correct. Try Again.");
+                }
+                else{
+                    $("#signInUsername").val("");
+                    $("#signInPass").val("");
+
+                    window.location = "main.html";
+
+                }
+
+            }
+    });
+*/
+}
--- js/index.js
+++ js/index.js
@@ -2,4 +2,50 @@
 $(document).ready(function() {
     $("header").load("header.html");
 
+    $(document).on('submit', "#signUpform", signUp);
 });
+
+
+
+function signUp(event){
+    event.preventDefault();
+    var password = $("#password").val();
+    var cpassword = $("#confirmPassword").val();
+
+    if(password !== cpassword){
+        alert("Passwords do not match. Try again.");
+        return;
+    }
+    else{
+        /*$.ajax({
+            type: "POST",
+            url: "api/Users",
+            data: {
+                firstname: $("#firstName").val(),
+                lastname: $("#lastName").val(),
+                username: $("#username").val(),
+                email: $("#email").val(),
+                password: $("#password").val()
+            },
+            success:function(json){
+                $.ajax({
+                    type: "POST",
+                    url: "api/Login",
+                    data: {
+                        email: $("#username").val(),
+                        password: $("#password").val()
+                    },
+                    success: function(json){
+                       window.location = "main.html";                       
+                    }
+            });
+
+                $("#firstName").val("");
+                $("#lastName").val("");
+                $("#username").val("");
+                $("#email").val("");
+                $("#password").val("");
+                $("#confirmPassword").val("");                
+        }});*/
+    }
+}
