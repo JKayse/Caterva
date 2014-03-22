39e463dad907d3a1a939ea3342de78116e9cf881
JKayse <jkayse@smu.edu>
2014-03-22 01:41:08 -0500

    Added the edit group/friend icon, added links, added get friend requests, and added the add friend and create event forms.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
+var userId;
+
 
 $(document).ready(function() {
     $("header").load("header.html");
     $(document).on('click', ".groupButton", openList);
     $(".event img").width($(".event img").height());
+    $(document).on('click', "#editFriends", goToEditPage);
+    $(document).on('click', "#addFriend", addFriend);
+    $(document).on('click', "#createEvent", addEvent);
 
+/*
+    $.ajax({url:"api/LoginStatus", success: function(json){
+        if(json !== 'null'){
+            json = JSON.parse(json);
+            userId = json.ID;    
+        }
+        else{
+            return;
+        }
+    }});   
+
+
+    $.ajax({url:"api/FriendRequests/" + userId, success: function(json2){
+        json2 = JSON.parse(json2);
+        var friendRequests = json2.friendRequests;
+        for(var i = 0; i < friendRequests.length ; i++){
+            var username = friendRequests[i].username;
+            var friendId = friendRequests[i].friendId;
+            var friend = "<div class='friendRequest' friendId=" + friendId + "><h4>Add "+ username +"?</h4><img src='img/greenCheck.png' alt='Green Check' title='No'><img src='img/redX.png' alt='Red X' title='No'></div>";
+            $("#friendRequestList").append(friend);
+        }
+
+    }}); 
+*/
 
 });
 
@@ -11,4 +40,21 @@ $(document).ready(function() {
 function openList(){
     $(this).toggleClass("clickedGroup");
     $(this).next().toggle();
+}
+
+
+function goToEditPage(){
+    window.location = "edit.html";
+}
+
+function addFriend(){
+    $("#blackScreenofDeath").show();
+    $("#popUp").show();
+    $("#enterFriend").show();
+}
+
+function addEvent(){
+    $("#blackScreenofDeath").show();
+    $("#popUp").show();
+    $("#enterEvent").show();
 }
