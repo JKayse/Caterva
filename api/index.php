5fb33a3c61f86bb6c21e315703816ca19fb6acbe
chrismhauser <chrismhauser@yahoo.com>
2014-04-13 16:45:38 -0500

    Made viewEvents use a left join for event where nobody has rsvpd yet

--- api/index.php
+++ api/index.php
@@ -622,7 +622,7 @@ function viewEvents() {
 	try {	
 		$db = getConnection();
 	
-		$sql = "SELECT e.EventId, e.EventName, e.UserId as OwnerId, e.StartTime, e.EndTime, e.EventDescription, e.Share FROM Events e INNER JOIN GuestList g ON e.EventId=g.EventId WHERE e.UserId=:userId OR g.UserId=:userId ORDER BY e.StartTime";
+		$sql = "SELECT e.EventId, e.EventName, e.UserId as OwnerId, e.StartTime, e.EndTime, e.EventDescription, e.Share FROM Events e LEFT JOIN GuestList g ON e.EventId=g.EventId WHERE e.UserId=:userId OR g.UserId=:userId ORDER BY e.StartTime";
 		$stmt = $db->prepare($sql);
 		$stmt->bindParam('userId', $userId);
 		$stmt->execute();
