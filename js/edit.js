16a8594fa166f485eb845682cc6542e23afaf849
Maryssa <mecrews@smu.edu>
2014-04-22 23:06:37 -0500

    ...

--- js/edit.js
+++ js/edit.js
@@ -44,8 +44,9 @@ $(document).ready(function() {
                 var friend = "<div class='friendie' friendId=" + userId+ "><img src='img/close2.png' class='deleteFriend' alt='Delete' title='Delete'><h3>" + firstname +" "+ lastname + "</h3></div>";
                 $("#friendList").append(friend);
                 var friendAdder = "<input type='checkbox' class='friendList' friendId=" + userId + " id='" + userId + "friend' title='Add Friend' name='Add Friends'><label for='" + userId + "friend'>"+ firstname +" "+ lastname + "</label><br>";
+                var friendEditAdder = "<input type='checkbox' class='friendList' friendId=" + userId + " id='" + userId + "friendEdit' title='Add Friend' name='Add Friends'><label for='" + userId + "friend'>"+ firstname +" "+ lastname + "</label><br>";
                 $("#listOfFriends").append(friendAdder);
-                $("#editedListOfFriends").append(friendAdder);
+                $("#editedListOfFriends").append(friendEditAdder);
             }});    
         }
 
@@ -94,7 +95,7 @@ function editGroupPopUp() {
 
     for(var i = 0; i < friendList.length; i++)
     {
-        $("#" + friendList[i] + "friend").prop('checked', true);
+        $("#" + friendList[i] + "friendEdit").prop('checked', true);
     }
 
     $("#blackScreenofDeath").show();
@@ -300,8 +301,9 @@ function updateFriendList() {
                 var friend = "<div class='friendie' friendId=" + userId+ "><img src='img/close2.png' class='deleteFriend' alt='Delete' title='Delete'><h3>" + firstname +" "+ lastname + "</h3></div>";
                 $("#friendList").append(friend);
                 var friendAdder = "<input type='checkbox' class='friendList' friendId=" + userId + " id='" + userId + "friend' title='Add Friend' name='Add Friends'><label for='" + userId + "friend'>"+ firstname +" "+ lastname + "</label><br>";
+                var friendAdder = "<input type='checkbox' class='friendList' friendId=" + userId + " id='" + userId + "friendEdit' title='Add Friend' name='Add Friends'><label for='" + userId + "friend'>"+ firstname +" "+ lastname + "</label><br>";
                 $("#listOfFriends").append(friendAdder);
-                $("#editedListOfFriends").append(friendAdder);
+                $("#editedListOfFriends").append(friendEditAdder);
             }});    
         }
 
